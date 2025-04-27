7c8667fff1d01aea552b9c4a4db7c06bf02e1af3
epxzzy <epxzzy66business@outlook.com>
2025-04-28 02:05:25 +0530

    there is a little man inside my screen

--- src/main/java/com/epxzzy/createsaburs/screen/KyberStationScreen.java
+++ src/main/java/com/epxzzy/createsaburs/screen/KyberStationScreen.java
@@ -9,7 +9,9 @@ import com.epxzzy.createsaburs.misc.SliderWidget;
 import com.epxzzy.createsaburs.networking.ModMessages;
 import com.epxzzy.createsaburs.networking.packet.ServerboundRecolourItemPacket;
 import com.epxzzy.createsaburs.utils.ModTags;
+import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractButton;
@@ -17,19 +19,25 @@ import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+
+import javax.annotation.Nullable;
 
 public class KyberStationScreen extends AbstractContainerScreen<KyberStationMenu> {
     private static final ResourceLocation RECOLOUR_TEXTURE =
             new ResourceLocation(createsaburs.MOD_ID, "textures/gui/kyber_recolour.png");
 
     private static final ResourceLocation STANCE_TEXTURE =
-            new ResourceLocation(createsaburs.MOD_ID, "textures/gui/kyber_stanced.png");
+            new ResourceLocation(createsaburs.MOD_ID, "textures/gui/kyber_stance.png");
 
 
     private static final ResourceLocation OFF_TOGGLE=
@@ -73,6 +81,9 @@ public class KyberStationScreen extends AbstractContainerScreen<KyberStationMenu
     private int TABLE_MODE = 0;
     //0 = recolour
     //1 = stance
+
+    private float xMouse;
+    private float yMouse;
 
     public void slotChanged() {
 
@@ -223,7 +234,6 @@ public class KyberStationScreen extends AbstractContainerScreen<KyberStationMenu
 
              */
         this.SelectTab();
-        this.updateTabs();
     }
 
     public void UpdateServerRecipe() {
@@ -389,12 +399,52 @@ public class KyberStationScreen extends AbstractContainerScreen<KyberStationMenu
     public void render(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
         renderBackground(guiGraphics);
         super.render(guiGraphics, mouseX, mouseY, delta);
+        if(selectedTab==0){
+            renderRecolourTab(guiGraphics,mouseX,mouseY,delta);
+        }
+        if(selectedTab==1){
+            renderEntityInInventoryFollowsMouse(guiGraphics, this.leftPos + 125, this.topPos + 70, 30, (float)(this.leftPos + 125) - this.xMouse, (float)(this.topPos + 70 - 50) - this.yMouse, this.minecraft.player);
+        }
+
+        //LIST.render(guiGraphics, mouseX, mouseY, delta);
+        renderTooltip(guiGraphics, mouseX, mouseY);
+
+
+        guiGraphics.pose().pushPose();
+        guiGraphics.pose().translate(0.0F, 0.0F, 100.0F);
+        this.xMouse = (float)mouseX;
+        this.yMouse = (float)mouseY;
+
+        /*
+        for(KyberTabButton recipebooktabbutton : this.tabButtons) {
+            if(recipebooktabbutton.tabID==0){
+                //recipebooktabbutton.setPosition(this.leftPos,this.topPos-(35)+5);
+            }
+            if(recipebooktabbutton.tabID==1){
+                //recipebooktabbutton.setPosition(this.leftPos,this.topPos-(2*35)+5);
+            }
+            recipebooktabbutton.renderWidget(guiGraphics, mouseX, mouseY, delta);
+        if(tabButtons != null) {
+            tabButtons.get(0).renderWidget(guiGraphics, mouseX, mouseY, delta);
+            tabButtons.get(1).renderWidget(guiGraphics, mouseX, mouseY, delta);
+        }
+            }
+         */
+
+        RECOLOUR_TAB_BUTTON.renderWidget(guiGraphics,mouseX,mouseY,delta);
+        STANCE_TAB_BUTTON.renderWidget(guiGraphics,mouseX,mouseY,delta);
+
+        guiGraphics.pose().popPose();
+    }
+
+
+    public void renderRecolourTab(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
         if (this.RGB_TOGGLE != null) this.RGB_TOGGLE.render(guiGraphics, mouseX, mouseY, delta);
         manShutYourBitchAssUp(!this.menu.canCraft());
 
         guiGraphics.blit(RGB_MODE?ON_TOGGLE:OFF_TOGGLE,this.leftPos-30, this.topPos+90, (float)0, (float)0, 20, 10, 20, 10);
 
-        if (menu.canCraft()&&selectedTab==0) {
+        if (menu.canCraft()) {
             if (!this.RGB_MODE) {
                 this.HUE_SLIDER.active = true;
                 this.SAT_SLIDER.active = true;
@@ -463,50 +513,9 @@ public class KyberStationScreen extends AbstractContainerScreen<KyberStationMenu
             //^ slider shit
         }
 
-        //LIST.render(guiGraphics, mouseX, mouseY, delta);
-        renderTooltip(guiGraphics, mouseX, mouseY);
 
-
-        guiGraphics.pose().pushPose();
-        guiGraphics.pose().translate(0.0F, 0.0F, 100.0F);
-
-        /*
-        for(KyberTabButton recipebooktabbutton : this.tabButtons) {
-            if(recipebooktabbutton.tabID==0){
-                //recipebooktabbutton.setPosition(this.leftPos,this.topPos-(35)+5);
-            }
-            if(recipebooktabbutton.tabID==1){
-                //recipebooktabbutton.setPosition(this.leftPos,this.topPos-(2*35)+5);
-            }
-            recipebooktabbutton.renderWidget(guiGraphics, mouseX, mouseY, delta);
-        if(tabButtons != null) {
-            tabButtons.get(0).renderWidget(guiGraphics, mouseX, mouseY, delta);
-            tabButtons.get(1).renderWidget(guiGraphics, mouseX, mouseY, delta);
-        }
-            }
-         */
-
-        RECOLOUR_TAB_BUTTON.renderWidget(guiGraphics,mouseX,mouseY,delta);
-        STANCE_TAB_BUTTON.renderWidget(guiGraphics,mouseX,mouseY,delta);
-
-        guiGraphics.pose().popPose();
     }
 
- void updateTabs() {
-        int i = (this.width - 147) / 2 - (this.widthTooNarrow ? 0:86) - 30;
-        int j = (this.height - 166) / 2 + 3;
-        int k = 27;
-        int l = 0;
-
-        /*
-        for(KyberTabButton recipebooktabbutton : this.tabButtons) {
-            KyberModes recipebookcategories = recipebooktabbutton.getCategory();
-                recipebooktabbutton.visible = false;
-                recipebooktabbutton.setPosition(i, j + 27 * l++);
-        }
-
-         */
-    }
    void initSliderStuff(){
        this.HUE_SLIDER = getSliderForColour(0, 360, "hue ", 1);
        this.SAT_SLIDER = getSliderForColour(0, 100, "sat ", 2);
@@ -584,4 +593,56 @@ public class KyberStationScreen extends AbstractContainerScreen<KyberStationMenu
 
     }
 
+    public static void renderEntityInInventoryFollowsMouse(GuiGraphics pGuiGraphics, int pX, int pY, int pScale, float p_275604_, float p_275546_, LivingEntity pEntity) {
+        float f = (float)Math.atan((double)(p_275604_ / 40.0F));
+        float f1 = (float)Math.atan((double)(p_275546_ / 40.0F));
+        // Forge: Allow passing in direct angle components instead of mouse position
+        renderEntityInInventoryFollowsAngle(pGuiGraphics, pX, pY, pScale, f, f1, pEntity);
+    }
+
+    public static void renderEntityInInventoryFollowsAngle(GuiGraphics pGuiGraphics, int pX, int pY, int pScale, float angleXComponent, float angleYComponent, LivingEntity pEntity) {
+        float f = angleXComponent;
+        float f1 = angleYComponent;
+        Quaternionf quaternionf = (new Quaternionf()).rotateZ((float)Math.PI);
+        Quaternionf quaternionf1 = (new Quaternionf()).rotateX(f1 * 20.0F * ((float)Math.PI / 180F));
+        quaternionf.mul(quaternionf1);
+        float f2 = pEntity.yBodyRot;
+        float f3 = pEntity.getYRot();
+        float f4 = pEntity.getXRot();
+        float f5 = pEntity.yHeadRotO;
+        float f6 = pEntity.yHeadRot;
+        pEntity.yBodyRot = 180.0F + f * 20.0F;
+        pEntity.setYRot(180.0F + f * 40.0F);
+        pEntity.setXRot(-f1 * 20.0F);
+        pEntity.yHeadRot = pEntity.getYRot();
+        pEntity.yHeadRotO = pEntity.getYRot();
+        renderEntityInInventory(pGuiGraphics, pX, pY, pScale, quaternionf, quaternionf1, pEntity);
+        pEntity.yBodyRot = f2;
+        pEntity.setYRot(f3);
+        pEntity.setXRot(f4);
+        pEntity.yHeadRotO = f5;
+        pEntity.yHeadRot = f6;
+    }
+
+    public static void renderEntityInInventory(GuiGraphics pGuiGraphics, int pX, int pY, int pScale, Quaternionf p_281880_, @Nullable Quaternionf pCameraOrientation, LivingEntity pEntity) {
+        pGuiGraphics.pose().pushPose();
+        pGuiGraphics.pose().translate((double)pX, (double)pY, 50.0D);
+        pGuiGraphics.pose().mulPoseMatrix((new Matrix4f()).scaling((float)pScale, (float)pScale, (float)(-pScale)));
+        pGuiGraphics.pose().mulPose(p_281880_);
+        Lighting.setupForEntityInInventory();
+        EntityRenderDispatcher entityrenderdispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        if (pCameraOrientation != null) {
+            pCameraOrientation.conjugate();
+            entityrenderdispatcher.overrideCameraOrientation(pCameraOrientation);
+        }
+
+        entityrenderdispatcher.setRenderShadow(false);
+        RenderSystem.runAsFancy(() -> {
+            entityrenderdispatcher.render(pEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, pGuiGraphics.pose(), pGuiGraphics.bufferSource(), 15728880);
+        });
+        pGuiGraphics.flush();
+        entityrenderdispatcher.setRenderShadow(true);
+        pGuiGraphics.pose().popPose();
+        Lighting.setupFor3DItems();
+    }
 }
