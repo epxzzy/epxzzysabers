03a7cb762251cec95bbb8cee1fd6be84241e8c85
epxzzy <epxzzy66business@outlook.com>
2025-06-29 00:41:29 +0530

    no create? nvm
    tried to remove create as a dependency but i failed, TODO: find a rendering liberary

---
+++ src/main/java/com/epxzzy/createsaburs/rendering/foundation/SimpleCustomRenderer.java
@@ -0,0 +1,26 @@
+package com.epxzzy.createsaburs.rendering.foundation;
+
+import com.epxzzy.createsaburs.rendering.foundation.CustomRenderedItemModelRenderer;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+
+public class SimpleCustomRenderer implements IClientItemExtensions {
+
+    protected CustomRenderedItemModelRenderer renderer;
+
+    protected SimpleCustomRenderer(CustomRenderedItemModelRenderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public static SimpleCustomRenderer create(Item item, CustomRenderedItemModelRenderer renderer) {
+        CustomRenderedItems.register(item);
+        return new SimpleCustomRenderer(renderer);
+    }
+
+    @Override
+    public CustomRenderedItemModelRenderer getCustomRenderer() {
+        return renderer;
+    }
+
+}
+
