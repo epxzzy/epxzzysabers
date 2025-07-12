344d4d45dc8983ae296bf1decd36afbda6e81d47
epxzzy <epxzzy66business@outlook.com>
2025-07-13 04:56:19 +0530

    untested balancing of the sabers for the second time

--- src/main/java/com/epxzzy/createsaburs/item/ModItems.java
+++ src/main/java/com/epxzzy/createsaburs/item/ModItems.java
@@ -21,12 +21,12 @@ public class ModItems {
                 float hue = 0.5f + (float)(0.1 * Math.sin(System.currentTimeMillis() / 300.0));
                 return style.withColor(Color.HSBtoRGB(hue, 1f, 1f));
             });
-    public static final RegistryObject<Item> Protosaber = items.register("protosabur", () -> new Protosaber(new Item.Properties().rarity(RARITY_KYBER), 6, 16, 1));
+    public static final RegistryObject<Item> Protosaber = items.register("protosabur", () -> new Protosaber(new Item.Properties().rarity(RARITY_KYBER), 5, 16, 1));
     //public static final RegistryObject<Item> protosabur2 = items.register("protwosabur", () -> new Protosaber(new ShieldItem.Properties().rarity(RARITY_KYBER), 4, 18, 1));
-    public static final RegistryObject<Item> SINGLE_BLADED_SABER = items.register("single_bladed_saber", () -> new SingleBladed(new Item.Properties().rarity(RARITY_KYBER), 4, 18, -2));
-    public static final RegistryObject<Item> DUAL_BLADED_SABER = items.register("dual_bladed_saber", () -> new Protosaber(new Item.Properties().rarity(RARITY_KYBER), 6, 16, -1));
-    public static final RegistryObject<Item> ROTARY_SABER = items.register("rotary_saber", () -> new RotarySaber(new Item.Properties().rarity(RARITY_KYBER), 5, 12, -1));
-    public static final RegistryObject<Item> CROSSGUARD_SABER = items.register("crossguard_saber", () -> new CrossguardSaber(new Item.Properties().rarity(RARITY_KYBER), 3, 10, -1));
+    public static final RegistryObject<Item> SINGLE_BLADED_SABER = items.register("single_bladed_saber", () -> new SingleBladed(new Item.Properties().rarity(RARITY_KYBER), 3, 18, -2));
+    public static final RegistryObject<Item> DUAL_BLADED_SABER = items.register("dual_bladed_saber", () -> new Protosaber(new Item.Properties().rarity(RARITY_KYBER), 5, 16, -1));
+    public static final RegistryObject<Item> ROTARY_SABER = items.register("rotary_saber", () -> new RotarySaber(new Item.Properties().rarity(RARITY_KYBER), 4, 12, -1));
+    public static final RegistryObject<Item> CROSSGUARD_SABER = items.register("crossguard_saber", () -> new CrossguardSaber(new Item.Properties().rarity(RARITY_KYBER), 2, 10, -1));
 
 
 
