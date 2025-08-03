00ecff7939dcf30511cd5fbd181835dd6062e49c
epxzzy <epxzzy66business@outlook.com>
2025-08-03 19:27:58 +0530

    pike parry stuff

--- src/main/java/com/epxzzy/createsaburs/item/ModItems.java
+++ src/main/java/com/epxzzy/createsaburs/item/ModItems.java
@@ -26,7 +26,7 @@ public class ModItems {
     public static final RegistryObject<Item> ROTARY_SABER = items.register("rotary_saber", () -> new RotarySaber(new Item.Properties().rarity(RARITY_KYBER), 3, 12, -1));
     public static final RegistryObject<Item> CROSSGUARD_SABER = items.register("crossguard_saber", () -> new CrossguardSaber(new Item.Properties().rarity(RARITY_KYBER), 2, 10, -1));
     public static final RegistryObject<Item> BLASTER_HYBRID = items.register("blaster_saber", () -> new BlasterHybrid(new Item.Properties().rarity(RARITY_KYBER), 2, 14, -2));
-    public static final RegistryObject<Item> SABER_PIKE = items.register("saber_pike", () -> new SaberPike(new Item.Properties().rarity(RARITY_KYBER), 2, 14, -2));
+    public static final RegistryObject<Item> SABER_PIKE = items.register("saber_pike", () -> new SaberPike(new Item.Properties().rarity(RARITY_KYBER), 4, 16, -1));
 
 
 
