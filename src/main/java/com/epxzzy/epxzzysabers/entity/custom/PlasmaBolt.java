6094b5e80f712fc2d2616e6ec40cd01c918cf639
epxzzy <epxzzy66business@outlook.com>
2025-09-07 18:15:57 +0530

    wiki shit

--- src/main/java/com/epxzzy/epxzzysabers/entity/custom/PlasmaBolt.java
+++ src/main/java/com/epxzzy/epxzzysabers/entity/custom/PlasmaBolt.java
@@ -7,6 +7,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.entity.projectile.ItemSupplier;
 import net.minecraft.world.item.ItemStack;
@@ -54,7 +55,10 @@ public class PlasmaBolt extends AbstractHurtingProjectile implements ItemSupplie
         if (!this.level().isClientSide) {
             Entity var10000 = result.getEntity();
             Entity var3 = this.getOwner();
-            var10000.hurt(this.damageSources().mobProjectile(this, var3 instanceof LivingEntity var2 ? var2 : null), 1.0F);
+            var10000.hurt(this.damageSources().mobProjectile(this, var3 instanceof LivingEntity var2 ? var2 : null), 2.0F);
+            if(var10000 instanceof Player pplayer){
+                //todo shti
+            }
         }
     }
     @Override
