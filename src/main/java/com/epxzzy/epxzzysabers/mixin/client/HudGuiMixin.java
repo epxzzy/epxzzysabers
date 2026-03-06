e3e3f4f72ba92a13c7cf9ee53f2a3e3b388a4024
epxzzy <epxzzy66business@outlook.com>
2026-03-06 09:36:48 +0530

    kms
    progress for #23

---
+++ src/main/java/com/epxzzy/epxzzysabers/mixin/client/HudGuiMixin.java
@@ -0,0 +1,29 @@
+package com.epxzzy.epxzzysabers.mixin.client;
+
+import com.epxzzy.epxzzysabers.epxzzySabers;
+import com.epxzzy.epxzzysabers.screen.HudStuffRenderer;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.GameType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public class HudGuiMixin {
+
+    @Inject(
+            method = "renderCrosshair",
+            at = @At(value= "TAIL"),
+            cancellable = true)
+    public void renderSaberStatusCrosshair(GuiGraphics pGuiGraphics, CallbackInfo ci) {
+        Gui that = ((Gui) (Object) this);
+        HudStuffRenderer.renderThings(pGuiGraphics, that);
+    }
+
+
+}
