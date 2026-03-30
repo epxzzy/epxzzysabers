60cb300f80cad192412218a26c02f1b3d08ba82c
epxzzy <epxzzy66business@outlook.com>
2026-03-30 05:53:52 +0530

    gotta make the values synced

--- src/main/java/com/epxzzy/epxzzysabers/screen/HudStuffRenderer.java
+++ src/main/java/com/epxzzy/epxzzysabers/screen/HudStuffRenderer.java
@@ -1,12 +1,14 @@
 package com.epxzzy.epxzzysabers.screen;
 
 import com.epxzzy.epxzzysabers.epxzzySabers;
+import com.epxzzy.epxzzysabers.item.types.RotarySaber;
 import com.epxzzy.epxzzysabers.util.PlayerHelperLmao;
 import net.minecraft.client.AttackIndicatorStatus;
 import net.minecraft.client.Options;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameType;
 
 public class HudStuffRenderer {
@@ -14,6 +16,7 @@ public class HudStuffRenderer {
     public static final ResourceLocation ATTACKS = epxzzySabers.asResource("textures/gui/attacks.png");
     public static final ResourceLocation BLOCK = epxzzySabers.asResource("textures/gui/block.png");
     public static final ResourceLocation BLOCKS = epxzzySabers.asResource("textures/gui/blocks.png");
+    public static final ResourceLocation ROTARY = epxzzySabers.asResource("textures/gui/rotary_cooldown.png");
 
     public static void renderStuff(GuiGraphics pGuiGraphics, Gui that) {
         Options options = that.minecraft.options;
@@ -32,9 +35,27 @@ public class HudStuffRenderer {
     public static void renderCrosshairHud(GuiGraphics pGuiGraphics, Gui that) {
         int j = that.screenHeight / 2 - 15;
         int k = that.screenWidth / 2 - 14;
-        PlayerHelperLmao MixinPlayer = (PlayerHelperLmao) that.minecraft.player;
+        Player play = that.minecraft.player;
+        PlayerHelperLmao MixinPlayer = (PlayerHelperLmao) play;
         int atk = MixinPlayer.getSaberAttackForm();
         int blk = MixinPlayer.getSaberBlockForm();
+
+        int f = MixinPlayer.getFlyCooldown() / RotarySaber.MAX_FLIGHT_COOLDOWN;
+
+        boolean canfly = MixinPlayer.getFlightDuration() == RotarySaber.MAX_FLIGHT_DURATION;
+
+        //pGuiGraphics.blit(ROTARY, k, j+ 16, 32, 0, 16, 7,16*3,7);
+
+        /*
+        if ( canfly) {
+            pGuiGraphics.blit(ROTARY, k, j, 32, 0, 16, 7,16*3,7);
+        }
+        if (f > 1.0F) {
+            int l = (int)(f * 17.0F);
+            pGuiGraphics.blit(ROTARY, k, j+16, 0, 0, 16, 7,16*3,7);
+            pGuiGraphics.blit(ROTARY, k, j+16, 16, 0, l, 7,16*3,7);
+        }
+         */
 
         if (MixinPlayer.getSaberAttackAnim() > 0) {
             //pGuiGraphics.blit(ATTACK, k, j, 0, 0, 29, 29, 29, 29);
