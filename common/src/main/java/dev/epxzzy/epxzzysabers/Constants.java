3a72a61ebd6d3774774ca896e9dc807d98e19518
epxzzy <epxzzy66business@outlook.com>
2026-09-12 19:51:49 +0530

    fabric forge and neo

---
+++ common/src/main/java/dev/epxzzy/epxzzysabers/Constants.java
@@ -0,0 +1,11 @@
+package dev.epxzzy.epxzzysabers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "epxzzysabers";
+	public static final String MOD_NAME = "epxzzysabers";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
